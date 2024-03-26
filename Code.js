a1991047f0f23837b774c1d43f3b91fdc2860ab2
Maccabee Levine <msl321@lehigh.edu>
2024-03-26 16:56:11 -0400

    Load breaking changes to console

--- Code.js
+++ Code.js
@@ -1,3 +1,23 @@
-function myFunction() {
-    var ui = SpreadsheetApp.getUi();
-  }
+const BASE_URL = 'https://api.github.com/';
+const OWNER = 'folio-org';
+const BREAKING_CHANGE_LABEL = 'breaking';
+const GET_OPTIONS = {};
+
+function loadBreakingChanges() {
+ 
+  let repo = 'mod-circulation';
+
+  let url = BASE_URL + 'search/issues'
+    + '?q=is:pr' 
+      + '%20' + 'label:' + BREAKING_CHANGE_LABEL 
+      + '%20' + 'repo:' + OWNER + '/' + repo;
+
+  let rawResponse = UrlFetchApp.fetch(url, GET_OPTIONS);
+  let response = JSON.parse(rawResponse.getContentText());
+
+  let prs = response.items;
+  prs.forEach(pr => {
+    console.log(pr);
+  });
+
+}
