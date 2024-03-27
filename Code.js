788a6c23416c45cca153d71ce049b7a316228d4f
Maccabee Levine <msl321@lehigh.edu>
2024-03-27 12:45:14 -0400

    Filter repos by prefix

--- Code.js
+++ Code.js
@@ -3,6 +3,7 @@ const OWNER = 'folio-org';
 const BREAKING_CHANGE_LABEL = 'breaking';
 const GET_OPTIONS = {};
 const BREAKING_CHANGE_REGEX = /##.?Breaking Change(?<change>[^#]*)/;
+const INCLUDE_REPOS = ['tech-council', 'mod-c', 'mod-i'];
 
 // eslint-disable-next-line no-unused-vars
 function loadBreakingChanges() {
@@ -21,9 +22,11 @@ function loadBreakingChanges() {
 
   let repos = loadReposList();
   repos.forEach(repo => {
-    Utilities.sleep(2000);
-    console.log(`Loading from repo ${repo}`)
-    loadFromRepo(repo);
+    if (includeRepo(repo)) {
+      Utilities.sleep(4000);
+      console.log(`Loading from repo ${repo}`)
+      loadFromRepo(repo);
+    }
   });
 }
 
@@ -39,6 +42,15 @@ function loadReposList() {
     allRepoNames = allRepoNames.concat(repoNames);
   }
   return allRepoNames;
+}
+
+function includeRepo(repo) {
+  for (const pattern of INCLUDE_REPOS) {
+    if (repo.startsWith(pattern)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function loadFromRepo(repo) {
