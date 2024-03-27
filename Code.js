b603a7c7adb37a493b3f433562d22414fe94209f
Maccabee Levine <msl321@lehigh.edu>
2024-03-26 20:17:25 -0400

    Write PRs to sheet

--- Code.js
+++ Code.js
@@ -3,8 +3,19 @@ const OWNER = 'folio-org';
 const BREAKING_CHANGE_LABEL = 'breaking';
 const GET_OPTIONS = {};
 
+// eslint-disable-next-line no-unused-vars
 function loadBreakingChanges() {
- 
+  let sheet = SpreadsheetApp.getActiveSheet();
+
+  // Row headings
+  if (sheet.getLastRow() < 1) {
+    sheet.appendRow([
+      'Date',
+      'PR',
+      'Title'
+    ]);
+  }
+
   let repo = 'mod-circulation';
 
   let url = BASE_URL + 'search/issues'
@@ -17,7 +28,22 @@ function loadBreakingChanges() {
 
   let prs = response.items;
   prs.forEach(pr => {
-    console.log(pr);
+    // console.log(pr);
+    writePr(pr, repo);
   });
+}
 
+function writePr(pr, repo) {
+  let sheet = SpreadsheetApp.getActiveSheet();
+  let date = new Date().toJSON().slice(0, 10);
+  sheet.appendRow([
+    date,
+    pr.number,
+    pr.title
+  ]);
+  let link = SpreadsheetApp.newRichTextValue()
+    .setText(`${repo}-${pr.number}`)
+    .setLinkUrl(pr.html_url)
+    .build()
+  sheet.getRange(sheet.getLastRow(), 2, 1, 1).setRichTextValue(link);
 }
